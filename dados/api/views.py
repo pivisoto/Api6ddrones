2a2135abd9b8979cf3ccd71ad3937b8a8446624d
Pedro Folkmann <pedro.o.folkmann@gmail.com>
2023-11-29 20:51:57 -0300

    att

--- dados/api/views.py
+++ dados/api/views.py
@@ -1,38 +1,35 @@
 from .models import *
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
-import requests
 from django.views.decorators.csrf import csrf_exempt
 
-@api_view(['POST'])
-def VerificaUsuarioExiste(requests):
-    try:
-        CadastraUsuario = True
-        info = requests.data
-        EmailCadastrado = usuario.objects.filter(email=info['email']).first()
-        if EmailCadastrado:
-            return JsonResponse({'mensagem': 'Este email já está cadastrado'},True)
-        else:
-            return CadastraUsuario
-    except: 
-        raise Exception("Erro na verificação de existencia do email")
 
 @api_view(['POST'])
-def CadastraUsuario(requests):
+def VerificaUsuarioExiste(request):
     try:
-        info = requests.data
-        CadastraUsuario = True
-        usuario.objects.create(
+        info = request.data
+        email_cadastrado = usuario.objects.filter(email=info['email']).first()
+        if email_cadastrado:
+            return JsonResponse({'mensagem': 'Este email já está cadastrado'}, status=200)
+        else:
+            CadastraUsuario(request)
+            return JsonResponse({'mensagem': 'Usuário não cadastrado'}, status=200)
+    except Exception as e:
+        return JsonResponse({'mensagem': f'Erro na verificação de existência do email: {str(e)}'}, status=500)
+
+def CadastraUsuario(request):
+    try:
+        info = request.data
+        usuario_obj = usuario.objects.create(
             nome=info['nome'],
             email=info['email'],
             senha=info['senha'],
             endereco=info['endereco'],
             celular=info['celular'],
-        )   
-        return JsonResponse({'Cadastro do Usuario efetuado',CadastraUsuario})
-    except:
-        CadastraUsuario = False
-        raise Exception("Erro ao cadastra usuario no banco",CadastraUsuario)
+        )
+        return JsonResponse({'mensagem': 'Cadastro do usuário efetuado', 'usuario_id': usuario_obj.id}, status=201)
+    except Exception as e:
+        return JsonResponse({'mensagem': f'Erro ao cadastrar usuário no banco: {str(e)}'}, status=500)
 
 @api_view(['POST'])
 def VerificaLogin(requests):
